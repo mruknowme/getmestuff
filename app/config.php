d1bf771a4a61e23b29c51c9c2fa2bb36abe9d98c
Max <valetovmax@gmail.com>
2017-01-15 14:26:36 +0500

    Basic setup
    Changes: index.php router preset, php header preset

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+    
+    //DB
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_DB", "getmestuff");
+    define("DB_PREFFIX", "gms_");
+
+    //PATHS
+    define("PROTOCOL", "http://");
+    define("BASE_URL", PROTOCOL."getmestuff/");
+?>
--- index.php
+++ index.php
@@ -3,5 +3,26 @@
     //echo $_SERVER['REDIRECT_URL']."<br>";
     //echo $_SERVER['REDIRECT_QUERY_STRING']."<br>";
 
+    require_once($_SERVER['DOCUMENT_ROOT']."/app/config.php");
+
+    $page = "";
+
+    if ($_SERVER['REDIRECT_URL'] == "/") $page = "home";
+
+    else {
+        $page = substr($_SERVER['REDIRECT_URL'], 1);
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",BASE_URL.$_SERVER['REQUEST_URI'])) $page = "home";
+    }
+
+    switch ($page) {
+        case "home":
+            echo "Require home page";
+            break;
+        case "about":
+            echo "Require about page";
+            break;
+        default: echo "Require home/error page";
+            
+    }
     
 ?>
